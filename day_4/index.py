f0126906f6bada50273be7f34a8b3fd7f553b575
Farrukh <augini@inbox.ru>
2021-12-05 02:29:09 +0500

    solve: day 4 part_2

--- day_4/index.py
+++ day_4/index.py
@@ -16,7 +16,6 @@ def check_bolds(board):
                 counter += 1
         # print(f"column {x} has {counter} bolds")
         if counter == 5:
-            print(board)
             return True
 
     # check rows
@@ -27,7 +26,6 @@ def check_bolds(board):
                 counter += 1
         # print(f"row {x} has {counter} bolds")
         if counter == 5:
-            print(board)
             return True
 
     return False
@@ -43,6 +41,7 @@ def find_sum(board):
     return sum
 
 
+# part 1
 def play_bingo(input):
     boards = []
 
@@ -74,4 +73,50 @@ def play_bingo(input):
                 return sum * current_number
 
 
-print(play_bingo(get_file_content("input.txt")))
+# print(play_bingo(get_file_content("input.txt")))
+
+
+# put all boards into stack as they win
+# pick the last board in the stack once all numbers have been used
+
+# part 2
+def play_bingo_2(input):
+    boards_stack = []
+    boards = []
+
+    numbers = input[0].split(",")
+    boards_1D = input[1 : len(input)]
+
+    temp = []
+    # convert 1D list into 2D list
+    for x in range(len(boards_1D)):
+        temp.append(boards_1D[x])
+        if x != 0 and (x + 1) % 5 == 0:
+            boards.append(temp)
+            temp = []
+
+    # loop through bingo numbers
+    current_number = 0
+    for x in numbers:
+        current_number = int(x)
+        for y in boards:
+            if x in y:
+                for m in range(len(y)):
+                    if y[m] == x:
+                        y[m] = y[m] + "*"
+        for a in range(0, len(boards), 5):
+            all_bold = check_bolds(boards[a : a + 5])
+            if all_bold:
+                # push board into stack if all bolds are found
+                boards_stack.append(boards[a : a + 5])
+                # remove the board from the list
+                boards = boards[0:a] + boards[a + 5 : len(boards)]
+
+                if len(boards) == 0:
+                    # if all boards have been removed, return the last board in the stack
+                    return (
+                        find_sum(boards_stack[len(boards_stack) - 1]) * current_number
+                    )
+
+
+print(play_bingo_2(get_file_content("input.txt")))
