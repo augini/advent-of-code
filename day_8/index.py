b8a411da29293f49c59c9e699cc863ea9d4b5d8e
Farrukh <augini@inbox.ru>
2021-12-08 16:15:48 +0500

    solve: day 8

--- day_8/index.py
+++ day_8/index.py
@@ -19,22 +19,98 @@ def count_instances(input):
 # print(count_instances(get_sample_seperator("input.txt", "\n")))
 
 def decode_digits(input):
-  first_half = decoded_outputs = []
+  first_half = []
+  second_half = []
+  decoded_outputs = []
 
   for x in input:
     first_half.append(x[0:x.index('|')-1])
-  
+
+  for x in range(len(first_half)):
+    first_half[x] = first_half[x].split(" ")
+
+  for x in input:
+    second_half.append(x[x.index('|')+2:len(x)])
+  for x in range(len(second_half)):
+    second_half[x] = second_half[x].split(" ")
+
   for values in first_half:
-    for i in values.split(" "):
+    temp = []
+    for i in values:
       if len(i) == 2:
-        decoded_outputs.append({"1":i})
+        temp.append({"1":i})
       elif len(i) == 4:
-        decoded_outputs.append({"4":i})
+        temp.append({"4":i})
       elif len(i) == 3:
-        decoded_outputs.append({"7":i})
+        temp.append({"7":i})
       elif len(i) == 7:
-        decoded_outputs.append({"8":i})
+        temp.append({"8":i})
+      elif len(i) == 5:
+        temp.append({"5":i})
+      elif len(i) == 6:
+        temp.append({"6":i})
 
-  return decoded_outputs
+    decoded_outputs.append(temp)
+    for m in decoded_outputs:
+      seven_segments = [x for x in m if x.get("7") != None][0].get("7")
+      four_segments = [x for x in m if x.get("4") != None][0].get("4")
 
-print(decode_digits(get_sample_seperator("sample_input.txt", "\n")))
+      for n in range(len(m)):
+        if m[n].get("6") != None:
+          old_value = m[n].get("6")
+
+          counter = 0
+          for i in old_value:
+            if i not in seven_segments+four_segments:
+              counter += 1
+              
+          # we got "9"
+          if counter == 1:
+             m[n] = {"9":old_value}
+          elif counter == 2:
+            length = len("".join(set(old_value+seven_segments)))
+            if length == 7:
+              # we got "6"
+              m[n] = {"6":old_value}
+            elif length == 6:
+              # we got "0"
+              m[n] = {"0":old_value}
+
+        if m[n].get("5") != None:
+          old_value = m[n].get("5")
+
+          length = len("".join(set(old_value+four_segments)))
+          if length == 7:
+              # we got "2"
+              m[n] = {"2":old_value}
+          elif length == 6:
+             length =len("".join(set(old_value+seven_segments)))
+             if length == 6:
+              # we got "5"
+              m[n] = {"5":old_value}
+             elif length == 5:
+              # we got 3
+              m[n] = {"3":old_value}
+
+  total_sum = 0
+  decimal = ""
+  
+  for x in range(len(second_half)):
+    for y in second_half[x]:
+      for m in range(len(decoded_outputs[x])):
+        for key, value in decoded_outputs[x][m].items():
+          if len(y) == len(value):
+            counter=0
+            for i in range(len(y)):
+              if y[i] in value:
+                counter += 1
+            if counter == len(y):
+              decimal += key
+
+    # print(second_half[x],decimal)
+    total_sum+=int(decimal)
+    decimal = ""
+
+  return total_sum
+
+print(decode_digits(get_sample_seperator("input.txt", "\n")))
