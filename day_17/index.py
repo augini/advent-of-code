567603cbe295d62360663f57206d42a7e5d197d4
Farrukh <augini@inbox.ru>
2022-01-04 20:47:58 +0500

    solve: day 17 part 2

--- day_17/index.py
+++ day_17/index.py
@@ -1,10 +1,4 @@
 
-
-
-
-from os import X_OK
-
-
 def shoot_probe(input):
   # store the min and max points
   x_range, y_range = [],[]
@@ -20,13 +14,13 @@ def shoot_probe(input):
       y_range = value.split("..")
   
   # store the coors that land in trench
-  matching_coors = []
+  matching_velocities = []
 
   # store the current highest y_coor for coors that land in trench
   max_y_height = 0
 
-  for y in range(100):
-    for x in range(1, 100):
+  for y in range(-300, 300):
+    for x in range(1, 300):
       x_velocity, y_velocity = x,y
 
       # initial coors
@@ -36,13 +30,13 @@ def shoot_probe(input):
       temp_y_heights = []
 
       while x_point <= int(x_range[1]) and y_point >= int(y_range[0]):
-        
+
         # store the height of the current coors if it is higher than max_y_height
         if y_point > max_y_height:
           temp_y_heights.append(y_point)
 
         if (y_point in range(int(y_range[0]), int(y_range[1])) and x_point in range(int(x_range[0]),int(x_range[1]))):
-          matching_coors.append(f"{x,y}")
+          matching_velocities.append(f"{x,y}")
 
           # update max_y_height with the max in y_height for this coordinate that landes in trench
           # if done previously, returns wrong answer because probe might go higher but that starting velocity might not land in a trench
@@ -59,11 +53,14 @@ def shoot_probe(input):
           x_velocity = x_velocity - 1 if x_velocity > 0 else 0
           y_velocity -=1
 
+  print(matching_velocities)
+  return len(matching_velocities)
 
-  return max_y_height
 
-
-sample_input = "target area: x=20..30 y=-10..-5"
-input = "target area: x=195..238 y=-93..-67"
+sample_input = "target area: x=20..31 y=-10..-4"
+input = "target area: x=195..239 y=-93..-66"
 
 print(shoot_probe(sample_input))
+
+# 144 -> too low
+# 145 -> too low
